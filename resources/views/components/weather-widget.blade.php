2b0b95d3afbe3fcca52f711b4c87f4538a93e1c9
Shota Aoki <ikoa.syo@gmail.com>
2022-09-25 23:22:39 +0900

    create template

---
+++ resources/views/components/weather-widget.blade.php
@@ -0,0 +1,82 @@
+<div class="w-128 mx-auto bg-gray-900 text-white text-sm rounded-lg overflow-hidden">
+            <div class="current-weather relative">
+                <div class="flex items-center justify-between px-4 py-6">
+                    <div class="flex items-center">
+                        <div>
+                            <div class="text-5xl font-semiboll">4&#176;C</div>
+                            <div class="text-gray-400">Feels like 5&#176;C </div>
+                        </div>
+                        <div class="ml-5">
+                            <div class="font-semibold">Overcast</div>
+                            <div class="text-gra-400">Tokyo, Japan</div>
+                        </div>
+                        
+                    </div>
+                    <div>icon</div>
+                </div>
+                <button class="absolute right-0 bottom-0 mb-2 mr-2 text-xs">Toggle</button>
+            </div>
+            <ul class="future-weather bg-gray-800 px-4 py-6 space-y-8">
+                <li class="grid grid-cols-weather items-center">
+                    <div class="text-gray-400">Tue</div>
+                    <div class="flex items-center">
+                        <div>icon</div>
+                        <div>Clear sky</div>
+                    </div>
+                    <div class="text-right text-xs">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+
+                    </div>
+                </li>
+                <li class="grid grid-cols-weather items-center">
+                    <div class="text-gray-400">Tue</div>
+                    <div class="flex items-center">
+                        <div>icon</div>
+                        <div>Clear sky</div>
+                    </div>
+                    <div class="text-right text-xs">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+
+                    </div>
+                </li>
+                <li class="grid grid-cols-weather items-center">
+                    <div class="text-gray-400">Tue</div>
+                    <div class="flex items-center">
+                        <div>icon</div>
+                        <div>Clear sky</div>
+                    </div>
+                    <div class="text-right text-xs">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+
+                    </div>
+                </li>
+                <li class="grid grid-cols-weather items-center">
+                    <div class="text-gray-400">Tue</div>
+                    <div class="flex items-center">
+                        <div>icon</div>
+                        <div>Clear sky</div>
+                    </div>
+                    <div class="text-right text-xs">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+
+                    </div>
+                </li>
+                <li class="grid grid-cols-weather items-center">
+                    <div class="text-gray-400">Tue</div>
+                    <div class="flex items-center">
+                        <div>icon</div>
+                        <div>Clear sky</div>
+                    </div>
+                    <div class="text-right text-xs">
+                        <div>4&#176;C</div>
+                        <div>4&#176;C</div>
+
+                    </div>
+                </li>
+                
+            </ul>
+        </div>
